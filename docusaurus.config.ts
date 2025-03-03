3eb428cc82ae00acad5ebd5d41af4268b5d9f926
Jimmy Horng <jimmy.h@gmicloud.ai>
2025-03-04 00:24:35 +0800

    CF-47: Use public URL

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -10,10 +10,10 @@ const config: Config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://psychic-adventure-v7nw1l5.pages.github.io/',
+  url: 'https://gmiswe.github.io/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/document-site/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
