5bfccc80d5a3be86c613f71760ade85934828424
Jimmy Horng <jimmy.h@gmicloud.ai>
2025-03-03 23:13:02 +0800

    CF-47: Update configs

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -82,7 +82,7 @@ const config: Config = {
         },
         {to: '/blog', label: 'Blog', position: 'left'},
         {
-          href: 'https://github.com/facebook/docusaurus',
+          href: 'https://github.com/GMISWE/document-site',
           label: 'GitHub',
           position: 'right',
         },
