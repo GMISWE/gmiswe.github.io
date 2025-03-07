298289b7315587796f1955c632164101953f0262
John Garcia <john.g@gmitec.net>
2025-03-07 10:35:01 -0800

    Update colors and paths

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -21,7 +21,7 @@ function HomepageHeader() {
           <Link
             className="button button--secondary button--lg"
             to="/docs/intro">
-            Docusaurus Tutorial - 5min ⏱️
+            GMI Cloud Tutorial - 5min ⏱️
           </Link>
         </div>
       </div>
